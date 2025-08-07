21b61fc1065ce439f572c92e9ae0ab2f00db470e
reggaebill <aarongerhart7@gmail.com>
2025-08-07 17:05:46 -0400

    Delete space-man-wgu directory

--- space-man-wgu/Scenes/terrain.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-#func _ready():
-	#var grapple = get_path("") # Or use $NodeB if it's a direct chil
-	#var target = grapple.get("target")
--- space-man-wgu/target_var.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var target: Vector2
